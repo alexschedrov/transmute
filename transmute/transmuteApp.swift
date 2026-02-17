818aa561cf5f629965adb92f582a48d4fb6f8d8f
Alex Schedrov <alexander.schedrov@gmail.com>
2026-02-17 18:44:20 -0500

    Fix settings

--- transmute/transmuteApp.swift
+++ transmute/transmuteApp.swift
@@ -12,6 +12,17 @@ struct transmuteApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var body: some Scene {
+        MenuBarExtra("Transmute", systemImage: "arrow.trianglehead.2.clockwise") {
+            SettingsLink {
+                Text("Settings…")
+            }
+            .keyboardShortcut(",")
+            Divider()
+            Button("Quit Transmute") {
+                NSApplication.shared.terminate(nil)
+            }
+            .keyboardShortcut("q")
+        }
         Settings {
             SettingsView()
         }
@@ -19,34 +30,14 @@ struct transmuteApp: App {
 }
 
 class AppDelegate: NSObject, NSApplicationDelegate {
-    private var statusItem: NSStatusItem!
     private var hotkeyService: HotkeyService!
 
     func applicationDidFinishLaunching(_ notification: Notification) {
-        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
-        if let button = statusItem.button {
-            button.image = NSImage(
-                systemSymbolName: "arrow.trianglehead.2.clockwise",
-                accessibilityDescription: "transmute"
-            )
-        }
-
-        let menu = NSMenu()
-        menu.addItem(NSMenuItem(title: "Settings…", action: #selector(openSettings), keyEquivalent: ","))
-        menu.addItem(NSMenuItem.separator())
-        menu.addItem(NSMenuItem(title: "Quit Transmute", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
-        statusItem.menu = menu
-
         AccessibilityService.promptIfNeeded()
 
         hotkeyService = HotkeyService {
             TransmutePanel.show()
         }
         hotkeyService.register()
-    }
-
-    @objc func openSettings() {
-        NSApp.activate(ignoringOtherApps: true)
-        NSApp.sendAction(Selector(("showSettingsWindow:")), to: nil, from: nil)
     }
 }
