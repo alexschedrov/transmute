ca42c23f62ce57dd3a1f1c4dcec50d3a2e6af9de
Alex Schedrov <alexander.schedrov@gmail.com>
2026-02-17 18:13:38 -0500

    Initial Commit

---
+++ transmute/transmuteApp.swift
@@ -0,0 +1,17 @@
+//
+//  transmuteApp.swift
+//  transmute
+//
+//  Created by Alex Schedrov on 2/17/26.
+//
+
+import SwiftUI
+
+@main
+struct transmuteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
